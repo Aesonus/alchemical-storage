0239082ad3f36dd372c797daa36a671cda2f4b86
Cory Laughlin <corylcomposinger@gmail.com>
2024-06-27 14:23:31 +0000

    Add modules to package init.

--- alchemical_storage/filter/__init__.py
+++ alchemical_storage/filter/__init__.py
@@ -1,9 +1,11 @@
 """Classes to apply where and order_by clauses to sqlalchemy queries."""
 
-from .filter import FilterMap, NullFilterMap, OrderByMap
+from alchemical_storage.filter import exc
+from alchemical_storage.filter.filter import FilterMap, NullFilterMap, OrderByMap
 
 __all__ = [
     "FilterMap",
     "OrderByMap",
     "NullFilterMap",
+    "exc",
 ]
