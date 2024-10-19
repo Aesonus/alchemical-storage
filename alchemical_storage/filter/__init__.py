c7a30e1a6685c16af0305a537255b93b00ec637a
Cory Laughlin <corylcomposinger@gmail.com>
2024-10-19 19:56:43 +0000

    docs: revise `filter` package docstring
    Use reference to `StatementVisitor` class in the api docs
    
    Refs #8

--- alchemical_storage/filter/__init__.py
+++ alchemical_storage/filter/__init__.py
@@ -1,5 +1,6 @@
-"""Package containing `StatementVisitor` classes to add ``where`` and ``order_by`` clauses to
-``sqlalchemy.Select`` instances in the ``DatabaseIndex.get`` method."""
+"""Package containing
+:class:`StatementVisitor<alchemical_storage.visitor.StatementVisitor>` classes to add
+``where`` filters and ``order_by`` criteria to ``sqlalchemy.Select`` instances."""
 
 from alchemical_storage.filter import exc
 from alchemical_storage.filter.filter import FilterMap, NullFilterMap, OrderByMap
