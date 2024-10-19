518afccfd9249cf979fbea77cd76e1388d9f055b
Cory Laughlin <corylcomposinger@gmail.com>
2024-10-19 16:07:38 +0000

    Change `filter` module docstrings

--- alchemical_storage/filter/__init__.py
+++ alchemical_storage/filter/__init__.py
@@ -1,4 +1,5 @@
-"""Classes to apply where and order_by clauses to sqlalchemy queries."""
+"""Package containing `StatementVisitor` classes to add ``where`` and ``order_by`` clauses to
+``sqlalchemy.Select`` instances in the ``DatabaseIndex.get`` method."""
 
 from alchemical_storage.filter import exc
 from alchemical_storage.filter.filter import FilterMap, NullFilterMap, OrderByMap
